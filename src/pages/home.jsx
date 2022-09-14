bc0a94f4fb9cb01ab80a203d9a06cf3bf9a0cbb2
brenma <brentmatterson@gmail.com>
2022-09-14 00:04:49 -0700

    listing members on home page

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -10,14 +10,12 @@ const Home = () => {
         return str.substring(0, 6) + "..." + str.substring(str.length - 4);
     };
 
-    //Initialize our editionDrop contract
     const editionDrop = useEditionDrop("0xaF44EA1Ea7444DcF975815bBDe877E33cA0A4A6E");
-    //State variable for us to know if user has our NFT.
     const [hasClaimedNFT, setHasClaimedNFT] = useState(false);
-    // isClaiming lets us easily keep a loading state while the NFT is minting.
     const [isClaiming, setIsClaiming] = useState(false);
-    
-    const [isAccredited, setIsAccredited] = useState(false)
+    const [isAccredited, setIsAccredited] = useState(false);
+    const [memberTokenAmounts, setMemberTokenAmounts] = useState([]);
+    const [memberAddresses, setMemberAddresses] = useState([]);
 
     useEffect(() => {
         // If they don't have a connected wallet, exit!
@@ -42,6 +40,60 @@ const Home = () => {
         };
         checkBalance();
     }, [address, editionDrop]);
+
+    
+    // This useEffect grabs all the addresses of our members holding our NFT.
+    useEffect(() => {
+        if (!hasClaimedNFT) {
+            return;
+        }
+    
+        // Just like we did in the 7-airdrop-token.js file! Grab the users who hold our NFT
+        // with tokenId 0.
+        const getAllAddresses = async () => {
+            try {
+                const memberAddresses = await editionDrop.history.getAllClaimerAddresses(0);
+                setMemberAddresses(memberAddresses);
+                console.log("🚀 Members addresses", memberAddresses);
+            } catch (error) {
+                console.error("failed to get member list", error);
+            }
+        };
+        getAllAddresses();
+    }, [hasClaimedNFT, editionDrop.history]);
+    
+    // This useEffect grabs the # of token each member holds.
+    useEffect(() => {
+        if (!hasClaimedNFT) {
+            return;
+        }
+
+        const getAllBalances = async () => {
+            try {
+                const amounts = await token.history.getAllHolderBalances();
+                setMemberTokenAmounts(amounts);
+                console.log("👜 Amounts", amounts);
+            } catch (error) {
+                console.error("failed to get member balances", error);
+            }
+        };
+        getAllBalances();
+    }, [hasClaimedNFT, token.history]);
+    
+    // Now, we combine the memberAddresses and memberTokenAmounts into a single array
+    const memberList = useMemo(() => {
+        return memberAddresses.map((address) => {
+        // We're checking if we are finding the address in the memberTokenAmounts array.
+        // If we are, we'll return the amount of token the user has.
+        // Otherwise, return 0.
+        const member = memberTokenAmounts?.find(({ holder }) => holder === address);
+    
+        return {
+            address,
+            tokenAmount: member?.balance.displayValue || "",
+        }
+        });
+    }, [memberAddresses, memberTokenAmounts]);
 
     const onValueChange = (e) => {    
         if(e.target.value === "Yes") {
@@ -89,7 +141,29 @@ const Home = () => {
             <div className="member-page">
                 <h1>DAO Member Page</h1>
                 <p>Congratulations on being a member</p>
-
+                <div>
+                    <div>
+                    <h2>Member List</h2>
+                    <table className="card">
+                        <thead>
+                            <tr>
+                                <th>Address</th>
+                                <th>Voting Power</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {memberList.map((member) => {
+                                return (
+                                <tr key={member.address}>
+                                    <td>{shortenAddress(member.address)}</td>
+                                    <td>{member.tokenAmount}</td>
+                                </tr>
+                                );
+                            })}
+                        </tbody>
+                    </table>
+                    </div>
+                </div>
             </div>
         );
     };
